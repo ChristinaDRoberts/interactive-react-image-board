cb586db7c7c9ee6033c7814990655c99db4f951d
Christina <Christinepeninsula@hotmail.com>
2019-02-20 17:17:25 -0500

    small details to add

--- src/App.js
+++ src/App.js
@@ -1,55 +1,141 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
 class ImageForm extends Component {
-    
-    render() {
-        return(
+    constructor(props){
+        super(props);
+        this.state = {
+            caption: '',
+            url: ''
 
-        )
-    }
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+        console.log(e.target)
+
+        if(e.target.name === 'url') {
+            this.setState({url: e.target.value})
+
+
+        }else if(e.target.name === 'caption') {
+            this.setState({caption : e.target.value})
+
+      }
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let picture = {caption: this.state.caption, url: this.state.url};
+
+    this.props.handleSubmit(picture);
+
+    this.setState({caption: '', url: ''});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {/*<label htmlFor="name">*/}
+          {/*User Name:*/}
+          {/*<input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange}/>*/}
+        {/*</label>*/}
+
+          <label htmlFor="caption">
+          Description:
+          <input type="text" name="caption" id="caption" value={this.state.caption} onChange={this.handleChange}/>
+        </label>
+
+          <label htmlFor="url">
+          Image URL:
+          <input type="text" name="url" id="url" value={this.state.url} onChange={this.handleChange}/>
+        </label>
+
+          <input type='submit' value='click me'/>
+      </form>
+    )
+  }
+
+
+
 }
 
 
-//child class bc I am putting it in my render in App
+/*//child class bc I am putting it in my render in App*/
 class PicturesList extends Component {
-    // constructor(props) {
-    //     super(props);
+    render(props) {
+        console.log('props', this.props);
+
+
+        let pictures = this.props.pictures.map(function(picture, index) {
+            return (
+                <div key={index}>
+                        <img src={picture.url}/>
+                        <p>{picture.caption}</p>
+                    </div>
+            )
+
+        });
 
 
 
-    render() {
+
         return (
             <ul>
-                <li>
+                {pictures}
+            </ul>
 
-                    <img className="pictureItems" />
+        )
+  }
 
-                </li>
-            </ul>)
-
-    }
 }
 
 
 
 //parent class
-class App extends Component {
+class ImageBoard extends Component {
     constructor(props) {
         super(props);
-        this.state = { }
+        this.state = {
+            pictures: [{caption: "Ocean Waves", url: "https://image.shutterstock.com/image-photo/pacific-ocean-260nw-611361698.jpg"},
+                {caption: "mountains", url: "https://images.pexels.com/photos/291732/pexels-photo-291732.jpeg?cs=srgb&dl=adventure-cold-daylight-291732.jpg&fm=jpg"},
+                {caption: "flower", url: "https://images.pexels.com/photos/87840/daisy-pollen-flower-nature-87840.jpeg?cs=srgb&dl=bloom-blossom-close-up-87840.jpg&fm=jpg"}]
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+
+
+    handleSubmit(picture){
+        let pictures = this.state.pictures.slice();
+        pictures.unshift(picture);
+        this.setState({pictures})
+
+    }
+
+
             render(){
+        console.log('this props', this.props)
                 return (
                     <div className="mainApp">
                         <h1 className="heading"> Thanks For Visiting Christina's React Picture Board</h1>
-                            <PicturesList/>
+                             <ImageForm  handleSubmit={this.handleSubmit}/>
+                            <PicturesList pictures={this.state.pictures}/>
+                            ]
                     </div>
             )
         }
-    }
+}
+
+
+export default ImageBoard;
 
 
 
@@ -58,46 +144,4 @@ class App extends Component {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-export default App;
-
-
-
-
-
-
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
 
